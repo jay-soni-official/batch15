1933ee5987a95e45fe47d3dc100a9f94f1c8cda9
suraj <surajbele51@gmail.com>
2025-12-30 08:36:50 +0530

    seperate files created

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "ami_id" {
+  default = "ami-0c398cb65a93047f2"
+}
+variable "instance_type" {
+  default = "t3.micro"
+}
+variable "key_name" {
+  default = "id_rsa"
+}
+variable "vpc_id" {
+  default = "vpc-082c0ea21bec713f1"
+}
