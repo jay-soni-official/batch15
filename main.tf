a1d8d2a2e9b5533803f5b5b9dfe1417439109c25
suraj <surajbele51@gmail.com>
2025-12-31 09:33:23 +0530

    instance and vpc module created and called

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
-locals {
-  instance_name = "my-instance"
-}
 terraform {
   backend "s3" {
     bucket = "batch151234"
@@ -8,22 +5,3 @@ terraform {
     key = "tfstate"
   }
 }
-data "aws_security_group" "mysg" {
-  filter {
-    name   = "vpc-id"
-    values =[var.vpc_id]
-  }
-  filter {
-    name   = "group-name"
-    values = ["mysg"]
-  }
-}
-resource "aws_instance" "myinstance" {
-  ami = var.ami_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [data.aws_security_group.mysg.id]
-  key_name = var.key_name
-  tags = {
-    Name = local.instance_name
-  }
-} 
